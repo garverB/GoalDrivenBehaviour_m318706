3026118119eb806ed5bee2b56f9578f1aa9f7365
bgv102 <m318706@alunos.ipb.pt>
2023-10-31 12:52:49 +0000

    First step of the Adding More Resources To The World chapter

--- GoalDrivenBehaviour_m318706/Assets/GetPatient.cs
+++ GoalDrivenBehaviour_m318706/Assets/GetPatient.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GetPatient : GAction
 {
+    GameObject resource;
     public override bool PrePerform()
     {
         target = GWorld.Instance.RemovePatient();
@@ -11,10 +12,28 @@ public class GetPatient : GAction
         {
             return false;
         }
+        resource = GWorld.Instance.RemoveCubicle();
+        if(resource != null)
+        {
+            inventory.AddItem(resource);
+        }
+        else
+        {
+            GWorld.Instance.AddPatient(target);
+            target = null;
+            return false;
+        }
+
+        GWorld.Instance.GetWorld().ModifyState("FreeCubicle", -1);
         return true;
     }
     public override bool PostPerform()
     {
+        GWorld.Instance.GetWorld().ModifyState("Waiting", -1);
+        if (target)
+        {
+            target.GetComponent<GAgent>().inventory.AddItem(resource);
+        }
         return true;
     }
 }
