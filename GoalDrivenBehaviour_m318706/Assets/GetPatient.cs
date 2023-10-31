14ba24fa38fec7a3cc9ec0d58a8c451d13e26ce2
bgv102 <m318706@alunos.ipb.pt>
2023-10-31 12:21:35 +0000

    Third step of the Adding Agents chapter

--- GoalDrivenBehaviour_m318706/Assets/GetPatient.cs
+++ GoalDrivenBehaviour_m318706/Assets/GetPatient.cs
@@ -6,6 +6,11 @@ public class GetPatient : GAction
 {
     public override bool PrePerform()
     {
+        target = GWorld.Instance.RemovePatient();
+        if(target == null)
+        {
+            return false;
+        }
         return true;
     }
     public override bool PostPerform()
